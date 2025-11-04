9614ddfdbbb7b7a8e3bd187fa3e5d93376cc23b1
seilah <princenoru@gmail.com>
2025-11-04 12:43:07 +0400

    004-streams-hw: refactor Titanic statistics script for improved readability; optimize handling of class-based passenger statistics

--- homework/titanicStats.js
+++ homework/titanicStats.js
@@ -5,76 +5,87 @@ import {isChild, isFemale, isMale, regexPattern} from "./helpers.js";
 
 let statsVariables = {
     isFirstLine: true,
+
+    passengersInFirstClass: 0, passengersInSecondClass: 0, passengersInThirdClass: 0,
+
     totalFares: 0,
 
-    totalFaresForFirstClass: 0,
-    totalFaresForSecondClass: 0,
-    totalFaresForThirdClass: 0,
+    totalFaresForFirstClass: 0, totalFaresForSecondClass: 0, totalFaresForThirdClass: 0,
 
-    averageFaresForFirstClass: 0,
-    averageFaresForSecondClass: 0,
-    averageFaresForThirdClass: 0,
+    averageFaresForFirstClass: 0, averageFaresForSecondClass: 0, averageFaresForThirdClass: 0,
 
-    totalSurvived: 0,
-    totalNotSurvived: 0,
+    totalSurvived: 0, totalNotSurvived: 0,
 
-    survivedMen: 0,
-    survivedWomen: 0,
-    survivedChildren: 0,
+    survivedMen: 0, survivedWomen: 0, survivedChildren: 0,
 
-    deadMen: 0,
-    deadWomen: 0,
-    deadChildren: 0,
+    deadMen: 0, deadWomen: 0, deadChildren: 0,
 }
 
 const fileStream = fs.createReadStream('./train.csv', 'utf8');
 const reader = readLine.createInterface({
-    input: fileStream,
-    crlfDelay: Infinity
+    input: fileStream, crlfDelay: Infinity
 })
 
 
 reader.on('line', (line) => {
-        if (statsVariables.isFirstLine === true) {
-            statsVariables.isFirstLine = false;
-            return;
-        }
-
-        let oneLineArray = line.split(regexPattern) // here I split ONE line to several CELLS
-
-        statsVariables.totalFares += +oneLineArray[9];
-        if (oneLineArray[2] === '1') {
-            statsVariables.totalFaresForFirstClass += +oneLineArray[9];
-        } else if (oneLineArray[2] === '2') {
-            statsVariables.totalFaresForSecondClass += +oneLineArray[9];
-        } else if (oneLineArray[2] === '3') {
-            statsVariables.totalFaresForThirdClass += +oneLineArray[9];
-        }
-
-        statsVariables.averageFaresForFirstClass = (statsVariables.totalFaresForFirstClass / oneLineArray.length).toFixed(2);
-        statsVariables.averageFaresForSecondClass = (statsVariables.totalFaresForSecondClass / oneLineArray.length).toFixed(2)
-        statsVariables.averageFaresForThirdClass = (statsVariables.totalFaresForThirdClass / oneLineArray.length).toFixed(2)
-
-        let survived = +oneLineArray[1];
-
-        if (survived === 1) {
-            statsVariables.totalSurvived++;
-
-            if (isChild) statsVariables.survivedChildren++;
-            if (isMale) statsVariables.survivedMen++;
-            if (isFemale) statsVariables.survivedWomen++;
-
-
-        } else if (survived === 0) {
-            statsVariables.totalNotSurvived++;
-            if (isChild) statsVariables.deadChildren++;
-            if (isMale) statsVariables.deadMen++;
-            if (isFemale) statsVariables.deadWomen++;
-        }
-
-        printTitanicStats();
+    if (statsVariables.isFirstLine === true) {
+        statsVariables.isFirstLine = false;
+        return;
     }
-)
+
+    let oneLineArray = line.split(regexPattern) // here I split ONE line to several CELLS
+
+    statsVariables.totalFares += +oneLineArray[9];
+
+    if (oneLineArray[2] === '1') {
+        statsVariables.totalFaresForFirstClass += +oneLineArray[9];
+        statsVariables.passengersInFirstClass++;
+    } else if (oneLineArray[2] === '2') {
+        statsVariables.totalFaresForSecondClass += +oneLineArray[9];
+        statsVariables.passengersInSecondClass++;
+    } else if (oneLineArray[2] === '3') {
+        statsVariables.passengersInThirdClass++;
+        statsVariables.totalFaresForThirdClass += +oneLineArray[9];
+    }
+
+    let survived = +oneLineArray[1];
+
+    if (survived === 1) {
+        statsVariables.totalSurvived++;
+
+        if (isChild(oneLineArray)) {
+            statsVariables.survivedChildren++;
+        }
+        if (isMale(oneLineArray)) {
+            statsVariables.survivedMen++;
+        }
+        if (isFemale(oneLineArray)) {
+            statsVariables.survivedWomen++;
+        }
+
+
+    } else if (survived === 0) {
+        statsVariables.totalNotSurvived++;
+        if (isChild(oneLineArray)) {
+            statsVariables.deadChildren++;
+        }
+        if (isMale(oneLineArray)) {
+            statsVariables.deadMen++;
+        }
+        if (isFemale(oneLineArray)) {
+            statsVariables.deadWomen++;
+        }
+    }
+
+})
+
+reader.on('close', () => {
+    statsVariables.averageFaresForFirstClass = (statsVariables.totalFaresForFirstClass / statsVariables.passengersInFirstClass).toFixed(2);
+    statsVariables.averageFaresForSecondClass = (statsVariables.totalFaresForSecondClass / statsVariables.passengersInSecondClass).toFixed(2)
+    statsVariables.averageFaresForThirdClass = (statsVariables.totalFaresForThirdClass / statsVariables.passengersInThirdClass).toFixed(2)
+
+    printTitanicStats();
+})
 
 function printTitanicStats() {
     console.log(`1. Total Fares: ${statsVariables.totalFares}`)
